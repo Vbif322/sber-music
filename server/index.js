73f947eafa6da9226bd2b99c9c8c4809ff6c5221
Vbif322 <hxdfue@yandex.ru>
2022-12-07 02:06:27 +0300

    files upload

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import multer from 'multer';
 
 import { registerValidator, loginValidator, songCreateValidator} from "./validations.js";
 
@@ -8,6 +9,7 @@ import { dbUrl } from './private.js';
 
 import {login, register, profile} from './controllers/UserController.js';
 import { create, getAll, getOne, remove, update } from './controllers/SongController.js';
+import handleValidationsErrors from './utils/handleValidationsErrors.js';
 
 mongoose
 .connect(dbUrl)
@@ -16,17 +18,35 @@ mongoose
 
 const app = express();
 
-app.use(express.json());
+const storage = multer.diskStorage({
+    destination: (_, __, cb) => {
+        cb (null, 'uploads');
+    },
+    filename: (_, file, cb) => {
+        cb(null, file.originalname);
+    }
+});
 
-app.post('/auth/login', loginValidator, login);
-app.post('/auth/register', registerValidator, register);
+const upload = multer({ storage });
+
+app.use(express.json());
+app.use('/uploads', express.static('uploads'));
+
+app.post('/auth/login',  loginValidator, handleValidationsErrors, login);
+app.post('/auth/register',  registerValidator, handleValidationsErrors, register);
 app.get('/auth/profile', checkAuth, profile);
+
+app.post('/upload', checkAuth, upload.single('song'), (req, res) => {
+    res.json({
+        url: `/uploads/${req.file.originalname}`
+    });
+});
 
 app.get('/songs', getAll);
 app.get('/songs/:id', getOne);
-app.post('/songs', checkAuth, songCreateValidator, create);
+app.post('/songs', checkAuth, songCreateValidator, handleValidationsErrors, create);
 app.delete('/songs/:id', checkAuth, remove);
-app.patch('/songs/:id', checkAuth, update);
+app.patch('/songs/:id', songCreateValidator, checkAuth, handleValidationsErrors, update);
 
 app.listen(8080, (err) =>{
     if (err){
