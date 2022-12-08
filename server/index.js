9e7c0cc974fc5be5d4f1babe067a624bc6784849
Vbif322 <hxdfue@yandex.ru>
2022-12-08 06:53:20 +0300

    install cors

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import multer from 'multer';
+import cors from 'cors';
 
 import { registerValidator, loginValidator, songCreateValidator} from "./validations.js";
 
@@ -30,6 +31,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 app.use(express.json());
+app.use(cors());
 app.use('/uploads', express.static('uploads'));
 
 app.post('/auth/login',  loginValidator, handleValidationsErrors, login);
